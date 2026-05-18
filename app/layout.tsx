4318ae3edfefd9fdc3ffd74020f42eb52a128433
p32nicky <nickdavies100@gmail.com>
2026-05-17 21:04:08 -0700

    Force light mode - fix dark mobile display

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={geist.variable}>
+    <html lang="en" className={geist.variable} style={{ colorScheme: "light" }}>
       <head>
         <Script
           src={`https://www.googletagmanager.com/gtag/js?id=${GA_IDS[0]}`}
